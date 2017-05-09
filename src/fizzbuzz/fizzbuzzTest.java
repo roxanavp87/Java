5c08bfa342f10fe9c999ced771dbb3f5a83410d5
roxanavp87 <roxanavp87@gmail.com>
2017-05-09 15:40:21 -0500

    -Fizzbuzz Test

---
+++ src/fizzbuzz/fizzbuzzTest.java
@@ -0,0 +1,27 @@
+package fizzbuzz;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * Created by roxana on 5/9/17.
+ */
+public class fizzbuzzTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        PrintStream output = System.out;
+
+        for(int i=1; i<=100; i++) {
+            if( (i % 3 == 0) && (i % 5 == 0)) {
+                output.println("FizzBuzz");
+            } else if(i % 3 == 0) {
+                output.println("Fizz");
+            } else if(i % 5 == 0) {
+                output.println("Buzz");
+            } else {
+                output.println(i);
+            }
+
+        }
+    }
+}
