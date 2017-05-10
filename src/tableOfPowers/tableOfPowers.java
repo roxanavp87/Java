3568b5c9852cd139a0fe1aa396935f2361f41664
roxanavp87 <roxanavp87@gmail.com>
2017-05-10 09:29:21 -0500

    -Table of Powers

---
+++ src/tableOfPowers/tableOfPowers.java
@@ -0,0 +1,29 @@
+package tableOfPowers;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * Created by roxana on 5/9/17.
+ */
+public class tableOfPowers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        PrintStream output = System.out;
+        int number;
+        String tab;
+        output.println("What number do you want to go up to?");
+        number = input.nextInt();
+        output.println("number  | squared   | cubed\n" +
+                "------  | -------   | -----");
+        for(int i= 1; i<=number; i++) {
+            if (i < 4) {
+                tab = "\t\t\t| ";
+            } else {
+                tab = "\t\t| ";
+            }
+            output.printf(i + "\t\t| " + (int)Math.pow(i, 2) + tab + (int)Math.pow(i, 3) + '\n');
+        }
+
+    }
+}
