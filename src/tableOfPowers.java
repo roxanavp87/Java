297faf80f8bffe1d7e9c262c0421c4ca8bf9ac3d
roxanavp87 <roxanavp87@gmail.com>
2017-05-10 16:11:11 -0500

    - table of powers using printf

--- src/tableOfPowers.java
+++ src/tableOfPowers.java
@@ -14,15 +14,10 @@ public class tableOfPowers {
         do {
             output.println("What number do you want to go up to?");
             number = input.nextInt();
-            output.println("number  | squared   | cubed\n" +
-                    "------  | -------   | -----");
+            output.println("number | squared | cubed\n" +
+                    "------ | ------- | -----");
             for (int i = 1; i <= number; i++) {
-                if (i < 4) {
-                    tab = "\t\t\t| ";
-                } else {
-                    tab = "\t\t| ";
-                }
-                output.printf(i + "\t\t| " + (int) Math.pow(i, 2) + tab + (int) Math.pow(i, 3) + '\n');
+                output.printf("%-7d| %-8d| %d\n", i, i*i, i*i*i);
             }
 
             output.println("Press Y if you want to try with other number, or N if you want to finish...");
