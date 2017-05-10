2f10782aeaa9734b536fa8492a186877c02e7369
roxanavp87 <roxanavp87@gmail.com>
2017-05-10 15:22:56 -0500

    - bonus exercise Pling Plang Plong

---
+++ src/PlingPlangPlong.java
@@ -0,0 +1,56 @@
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by roxana on 5/10/17.
+ */
+public class PlingPlangPlong {
+    public static void main(String[] args) {
+//        Convert a number to a string, the contents of which depend on the number's factors.
+//
+//        If the number has 3 as a factor, output 'Pling'.
+//                If the number has 5 as a factor, output 'Plang'.
+//                If the number has 7 as a factor, output 'Plong'.
+//                If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+//
+//        Examples
+//
+//        28's factors are 1, 2, 4, 7, 14, 28.
+//        In raindrop-speak, this would be a simple "Plong".
+//        30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
+//        In raindrop-speak, this would be a "PlingPlang".
+//        34 has four factors: 1, 2, 17, and 34.
+//        In raindrop-speak, this would be "34".
+        Scanner input = new Scanner(System.in);
+        PrintStream output = System.out;
+        int num, factor;
+        String result = "";
+
+        output.println("Enter a number:");
+        num = input.nextInt();
+
+        ArrayList factors = math.factorsOfInteger.factors(num);
+        int listSize = factors.size();
+
+
+        output.println(num + "'s factors are: " + factors);
+
+        for(int i=0; i<listSize ; i++) {
+            factor = (int)factors.get(i);
+            if (factor == 3) {
+                result += "Pling";
+            } else if(factor == 5) {
+                result += "Plang";
+            } else if(factor == 7) {
+                result += "Plong";
+            }
+        }
+
+        if(result.equals("")) {
+            result += (int)factors.get(listSize-1);
+        }
+
+        output.println("In raindrop-speak, this would be: " + result);
+    }
+}
