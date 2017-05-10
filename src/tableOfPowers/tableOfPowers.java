80990ea291997878b4e55526aae6e2abc8682614
roxanavp87 <roxanavp87@gmail.com>
2017-05-10 09:35:13 -0500

    -Table of Powers finished

--- src/tableOfPowers/tableOfPowers.java
+++ src/tableOfPowers/tableOfPowers.java
@@ -11,19 +11,24 @@ public class tableOfPowers {
         Scanner input = new Scanner(System.in);
         PrintStream output = System.out;
         int number;
-        String tab;
-        output.println("What number do you want to go up to?");
-        number = input.nextInt();
-        output.println("number  | squared   | cubed\n" +
-                "------  | -------   | -----");
-        for(int i= 1; i<=number; i++) {
-            if (i < 4) {
-                tab = "\t\t\t| ";
-            } else {
-                tab = "\t\t| ";
-            }
-            output.printf(i + "\t\t| " + (int)Math.pow(i, 2) + tab + (int)Math.pow(i, 3) + '\n');
-        }
+        String tab, condition;
 
+        do {
+            output.println("What number do you want to go up to?");
+            number = input.nextInt();
+            output.println("number  | squared   | cubed\n" +
+                    "------  | -------   | -----");
+            for (int i = 1; i <= number; i++) {
+                if (i < 4) {
+                    tab = "\t\t\t| ";
+                } else {
+                    tab = "\t\t| ";
+                }
+                output.printf(i + "\t\t| " + (int) Math.pow(i, 2) + tab + (int) Math.pow(i, 3) + '\n');
+            }
+
+            output.println("Press Y if you want to try with other number, or N if you want to finish...");
+            condition = input.next();
+        } while(condition.equalsIgnoreCase("y"));
     }
 }
