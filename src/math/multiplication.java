9ce800cb171dabe540d62eb70bcd07aa7192ab96
roxanavp87 <roxanavp87@gmail.com>
2017-05-11 17:20:26 -0500

    - multiplication method using recursion

---
+++ src/math/multiplication.java
@@ -0,0 +1,15 @@
+package math;
+
+/**
+ * Created by roxana on 5/11/17.
+ */
+public class multiplication {
+    public static int multWithRecursion(int num1, int num2) {
+        if(num2 == 0) {
+            return 0;
+        } else if(num2 == 1) {
+            return num1;
+        }
+        return num1 + multWithRecursion(num1, num2 -1);
+    }
+}
