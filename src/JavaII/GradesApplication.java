9352649cab8f9dd3ec995af835b6871101f1c6ab
roxanavp87 <roxanavp87@gmail.com>
2017-05-22 16:31:50 -0500

    exercise 3: working with HashMap - print username of students -display info of students

--- src/JavaII/GradesApplication.java
+++ src/JavaII/GradesApplication.java
@@ -2,6 +2,7 @@ package JavaII;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by roxana on 5/22/17.
@@ -20,11 +21,23 @@ public class GradesApplication {
     public void printKey() {
         students.forEach((k, v) -> System.out.print("| "+ k + "| "));
     }
+
+    public void printStudentInfo(String key) {
+        if(students.get(key) != null) {
+            System.out.println("Name: " + students.get(key).getName() +
+                    "\nCurrent Average: " + students.get(key).getGradeAverage());
+        } else {
+            System.out.println("Sorry, no student found with the gihub username of " + key);
+        }
+    }
 }
 
 
 class TestingGradesapplication {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String userInput;
+
         Student student1 = new Student("rox");
         student1.addGrade(4);
         student1.addGrade(5);
@@ -44,6 +57,16 @@ class TestingGradesapplication {
         System.out.println("Here are the github usernames of our students:");
         grades.printKey();
 
+        do {
+            System.out.println("\nWhat student would you like to see more information on?");
+            userInput = input.nextLine();
+            grades.printStudentInfo(userInput);
+
+            System.out.println("Would you like to see another student? (y/n)");
+            userInput = input.nextLine();
+        } while (userInput.equalsIgnoreCase("y"));
+
+        System.out.println("Goodbye, and have a wonderful day!");
         
     }
 }
