37c22bf652852699f7a2c6f6344efd203ac94ba1
roxanavp87 <roxanavp87@gmail.com>
2017-05-22 14:13:33 -0500

    - create the following methods: getName(), addGrade(int grade), getGradeAverage()

--- src/JavaII/Student.java
+++ src/JavaII/Student.java
@@ -1,16 +1,35 @@
 package JavaII;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Created by roxana on 5/22/17.
  */
 public class Student {
     private String name;
-    ArrayList<Integer> grades;
+    private List<Integer> grades;
 
     public Student(String name) {
         this.name = name;
         this.grades = new ArrayList<Integer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        int sum = 0;
+        for(Integer grade : grades) {
+            sum += grade;
+        }
+
+        return sum/(double)grades.size();
     }
 }
